91fa2541ca74d84608cde1e78dfbeec6b06d5265
Wojtek <wojtekj1989@gmail.com>
2019-01-30 20:05:04 +0100

    created album list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { SearchComponent } from "./search/search.component";
+import { AlbumListComponent } from './album-list/album-list.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchComponent],
+  declarations: [AppComponent, SearchComponent, AlbumListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
