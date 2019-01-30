336096ac73c4bce43a298172ab011b058e3ced2b
Wojtek <wojtekj1989@gmail.com>
2019-01-30 20:55:44 +0100

    structured files and pulled data from api

--- src/app/album-list/album-list.component.ts
+++ src/app/album-list/album-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Album } from "../models/album.model";
+import { AlbumsService } from "../services/albums.service";
 
 @Component({
-  selector: 'app-album-list',
-  templateUrl: './album-list.component.html',
-  styleUrls: ['./album-list.component.scss']
+  selector: "app-album-list",
+  templateUrl: "./album-list.component.html",
+  styleUrls: ["./album-list.component.scss"]
 })
 export class AlbumListComponent implements OnInit {
+  albums: Album[] = [];
 
-  constructor() { }
+  constructor(private as: AlbumsService) {}
 
   ngOnInit() {
+    this.as.getAlbums().subscribe(result => {
+      result.feed.entry.forEach(element => {
+        let album: Album = new Album(
+          element["im:name"].label,
+          element["im:image"][2].label,
+          element["im:releaseDate"].label,
+          element["im:artist"].label
+        );
+        this.albums.push(album);
+      });
+    });
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { SearchComponent } from "./search/search.component";
-import { AlbumListComponent } from './album-list/album-list.component';
+import { AlbumListComponent } from "./album-list/album-list.component";
+import { AlbumItemComponent } from "./album-item/album-item.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [AppComponent, SearchComponent, AlbumListComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    AlbumListComponent,
+    AlbumItemComponent
+  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
